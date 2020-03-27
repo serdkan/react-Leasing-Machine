64356eb6988cd46959bc436a1d0508ef06700a8f
serdkan <serdar@ozgurce.com>
2020-03-27 16:54:23 +0300

    react first
    first file

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import MainPage from './views/Layouth'
+
+function App() {
+  return (
+    <div className="App">
+          <MainPage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/views/Layouth.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import Ustmenu from './Navbar'
+ class layouth extends Component {
+    render() {
+        return (
+            <div>
+                 <Ustmenu></Ustmenu>
+            </div>
+        )
+    }
+}
+
+export default layouth;
